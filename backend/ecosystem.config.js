473a80a7288a1973b8a5eeea572693517996ada8
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-25 22:20:13 +0300

    реализация компановки приложения через docker compose

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,10 +1,11 @@
 const { JWT_SECRET, POSTGRES_DB,  POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT} = process.env;
+console.log(POSTGRES_DB)
 
 module.exports = {
   apps: [
     {
       name: 'kpd-backend',
-      script: './dist/app.js',
+      script: './dist/main.js',
       env_production: {
         NODE_ENV: 'production',
         POSTGRES_DB,
