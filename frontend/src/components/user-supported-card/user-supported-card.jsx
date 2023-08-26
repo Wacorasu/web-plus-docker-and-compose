fbdc49fc9a63b16c63d4e1c8d376e388a56d36d6
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-26 22:48:31 +0300

    bugfix id

--- frontend/src/components/user-supported-card/user-supported-card.jsx
+++ frontend/src/components/user-supported-card/user-supported-card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./user-supported-card.module.css";
+import { URL } from "../../utils/constants";
 
 export const UserSupportedCard = ({
   name = "",
