7f1383fa5c30c50cb77b81e72ea335f9264bb6cf
Constantin Vanchurenkov <i@wacorasu.ru>
2023-08-26 20:29:12 +0300

    bugfix id

--- backend/src/offers/entity/offer.entity.ts
+++ backend/src/offers/entity/offer.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, ManyToOne } from 'typeorm';
 import { PrimaryBaseModel } from 'src/utils/base-entity/primary-base-model.entity';
-import { OFFER_HIDDEN_DEFAULT } from 'src/utils/constants';
+import { OFFER_HIDDEN_DEFAULT, USER_ABOUT_DEFAULT } from 'src/utils/constants';
 import { User } from 'src/users/entity/user.entity';
 import { Wish } from 'src/wishes/entity/wish.entity';
 import { Length } from 'class-validator';
@@ -10,7 +10,7 @@ export class Offer extends PrimaryBaseModel {
   @ManyToOne(() => User, (user) => user.offers)
   user: User;
 
-  @Column()
+  @Column({ default: USER_ABOUT_DEFAULT })
   @Length(0, 250)
   username: string;
 
